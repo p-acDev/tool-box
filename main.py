6240c856b522533e8b615d248e66a86c173647da
pacourbet <pierreandre.courbet@gmail.com>
2022-03-18 09:59:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,63 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 
-# to be chosen by user
-data = st.file_uploader("Upload raw data")
+st.title("Tool box")
 
-if st.button("Interpoler") and data:
 
-    df = pd.read_csv(data, sep=";")
 
-    with st.expander("Clicker pour voir vos données"):
-        st.dataframe(df)
+## interpolation app
+chosen_app = st.sidebar.selectbox(
+    "Selectionner l'app que vous voulez utiliser",
+    ("-", "Interpolation", "Other")
+)
 
-    x, y = df['x'], df['y']
+if chosen_app == "-":
+    ## Introduction
+    with st.container():
 
-    # to be chosen by user
-    x_new = np.arange(0, 360, 30)
+        st.info("""
+        Tool Box, une web app qui rassemble
+        plusieurs outils que j'utilise régulièrement""")
 
-    y_new = np.interp(x_new, x, y)
+elif chosen_app == "Interpolation":
 
-    # plt.plot(x, y, label="raw data")
-    # plt.plot(x_new, y_new, label="interpolated")
-    # plt.legend()
-    # plt.xticks(x_new)
-    # plt.grid()
+    with st.container():
+        st.subheader("Interpolation")
+        # to be chosen by user
+        st.info("Application pour interpoler un jeu de donnéess")
+        data = st.file_uploader("Upload raw data")
 
-    st.success("Voici votre interpolation")
+        if st.button("Interpoler") and data:
 
-    # st.pyplot(plt)
+            df = pd.read_csv(data, sep=";")
 
-    df_interp = pd.DataFrame()
-    df_interp["x"] = x_new
-    df_interp["y"] = y_new
+            with st.expander("Clicker pour voir vos données"):
+                st.dataframe(df)
 
-    st.dataframe(df_interp)
-    st.download_button(
-        label="Download data as csv",
-        data=df_interp.to_csv(),
-        file_name="interpolated_data.csv")
-        
+            x, y = df['x'], df['y']
+
+            # to be chosen by user
+            x_new = np.arange(0, 360, 30)
+
+            y_new = np.interp(x_new, x, y)
+
+            # plt.plot(x, y, label="raw data")
+            # plt.plot(x_new, y_new, label="interpolated")
+            # plt.legend()
+            # plt.xticks(x_new)
+            # plt.grid()
+
+            st.success("Voici votre interpolation")
+
+            # st.pyplot(plt)
+
+            df_interp = pd.DataFrame()
+            df_interp["x"] = x_new
+            df_interp["y"] = y_new
+
+            st.dataframe(df_interp)
+            st.download_button(
+                label="Download data as csv",
+                data=df_interp.to_csv(),
+                file_name="interpolated_data.csv")
+                
