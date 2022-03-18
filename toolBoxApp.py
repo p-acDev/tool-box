ae98cd7c27ce0ad6ff9f0a2f1a49c724c9c61817
pacourbet <pierreandre.courbet@gmail.com>
2022-03-18 12:50:38 +0100

    Update toolBoxApp.py

--- toolBoxApp.py
+++ toolBoxApp.py
@@ -10,7 +10,7 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="expanded",
      menu_items={
-         'Documentation': 'https://the-tool-box.readthedocs.io/en/latest/'
+         'About': "https://the-tool-box.readthedocs.io/en/latest/"
      }
  )
 
