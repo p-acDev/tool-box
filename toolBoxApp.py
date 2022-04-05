bd5eb1af95e21c4d359b8c5343c8ba1153e90a31
pacourbet <pierreandre.courbet@gmail.com>
2022-04-05 14:50:11 +0200

    remove footer custom css
    just keep it simple for the moment

--- toolBoxApp.py
+++ toolBoxApp.py
@@ -38,35 +38,3 @@ else :
 
         app_gui = importlib.import_module(f'.{chosen_app.lower()}.gui' ,'.apps').Gui(app__info)
         app_gui.create()
-
-
-# custome css
-
-footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: white;
-color: black;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed with ❤ by <a style='display: block; text-align: center;' href="www.pacourbet.net" target="_blank">p-a.c</a></p>
-</div>
-"""
-st.markdown(footer,unsafe_allow_html=True)
